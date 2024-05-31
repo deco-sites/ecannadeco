4f100d283feeaed7841d030dea76d8e2bb28dce1
riquezzz <riquezzz@riquezzzs-MacBook-Pro.local>
2024-05-31 12:46:59 -0300

    buscar pacientes

--- actions/prescriberGetPatients.ts
+++ actions/prescriberGetPatients.ts
@@ -1,20 +1,23 @@
 export interface Props {
   token: string;
+  search?: string;
 }
 
 const prescriberGetPetients = async (
-  { token }: Props,
+  { token, search }: Props,
   _req: Request,
 ): Promise<unknown | null> => {
   try {
+    const query = search ? `?search=${search}` : "";
     const response = await fetch(
-      `https://api.ecanna.com.br/prescribers/patients`,
+      `https://api.ecanna.com.br/prescribers/patients${query}`,
       {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
           Authorization: token,
         },
+        
       },
     );
     const res = await response.json();
