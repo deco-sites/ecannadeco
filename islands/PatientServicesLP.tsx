9af8be1bb1b7d04bb56419ea92072f0839a626ef
Guilherme Souto <baufaker@gmail.com>
2024-06-08 00:55:56 -0300

    changed lp to island and fixed validatePAssword in signup

---
+++ islands/PatientServicesLP.tsx
@@ -0,0 +1,8 @@
+import type { Props } from "../components/landingpages/PatientServicesLP.tsx";
+import Component from "../components/landingpages/PatientServicesLP.tsx";
+
+function Island(props: Props) {
+  return <Component {...props} />;
+}
+
+export default Island;
